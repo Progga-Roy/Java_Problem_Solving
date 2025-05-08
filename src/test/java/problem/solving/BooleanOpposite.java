1d5b98d38ced5ebc6b8f33132243738a809c9a42
Progga-Roy <proggaroy2000@gmail.com>
2025-05-08 21:36:23 +0600

    Problem 3: Boolean value opposite.

---
+++ src/test/java/problem/solving/BooleanOpposite.java
@@ -0,0 +1,16 @@
+//Write a program that takes a boolean value as input (true/false) and prints its opposite value.
+
+package problem.solving;
+
+import java.util.Scanner;
+
+public class BooleanOpposite {
+    public static void main(String[] args){
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter True/False : ");
+        boolean input = userInput.nextBoolean();
+        boolean opposite = !input;
+        System.out.println(opposite);
+
+    }
+}
